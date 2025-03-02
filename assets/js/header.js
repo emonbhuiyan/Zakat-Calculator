62dfc170327d2f034cc0b6f1063be6aa00261498
Emon <mdemonbhuiyan@live.com>
2025-03-02 13:25:08 +0200

    Initial commit

---
+++ assets/js/header.js
@@ -0,0 +1,9 @@
+// assets/js/header.js
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("components/header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header-container").innerHTML = data;
+        })
+        .catch(error => console.error("Error loading header:", error));
+});
